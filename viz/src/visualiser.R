2d19e35b6fbe15d1eef268b5d4d6ab775b0d5546
cecilia <cecilia.aas@cantab.net>
2017-07-01 22:49:22 +0100

    visualiser with bigger data

--- viz/src/visualiser.R
+++ viz/src/visualiser.R
@@ -17,16 +17,22 @@ setwd("/home/cecilia/Documents/hackathons/viz/src")
 tags = read.csv("../data/test_data.txt")
 names = read.csv("../data/names.txt")
 states = read.csv("../data/states.txt")
-data_cleaned = read.csv2(file="../../refined_data/clean_data.txt", sep=",", dec=".", header=FALSE, colClasses=c(NA, NA, NA, NA, NA, "Date"))
-
+data_cleaned = read.csv2(file="../../refined_data/clean_data.csv", sep=",", dec=".", header=FALSE, colClasses=c(NA, NA, NA, NA, NA, "Date"))
 
 data <- data.frame(
-  id      = 1:4,
+  id      = 1:300,
   content = c("Item one", "Item two",
               "Ranged item", "Item four"),
   start   = c("2016-01-10", "2016-01-11",
               "2016-01-20", "2016-02-14 15:00:00"),
-  end     = c(NA, NA, "2016-02-04", NA)
+  end     = c(NA, NA, NA, NA)
+)
+
+data_dates <- data.frame(
+  id      = 1:300,
+  content = data_cleaned$V1,
+  start   = data_cleaned$V5,
+  end     = c(rep(NA, 300))
 )
 
 
@@ -34,32 +40,29 @@ data <- data.frame(
 ui <- fluidPage(
     
     titlePanel("eJuris"),
-    
-    sidebarLayout(
-      
-      sidebarPanel(
-        # Inputs excluded for brevity
-      ),
-      
+
       mainPanel(
         tabsetPanel(
           tabPanel("Dates", timevisOutput("timeline")),
-          tabPanel("Places", leafletOutput("mymap"), p(), actionButton("recalc", "New points")),
+          tabPanel("Places", leafletOutput("mymap"), p()),
           tabPanel("People", tableOutput("table"))
         )
       )
-    )
 )
 
 server <- function(input, output, session) {
   output$timeline <- renderTimevis({
-    timevis(data)
+    timevis(data_dates)
   })
   
-  points <- eventReactive(input$recalc, {
-    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-  }, ignoreNULL = FALSE)
+  #points <- eventReactive(input$recalc, {
+  #  cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+  #}, ignoreNULL = FALSE)
   
+  points <- eventReactive(input$recalc, {
+      cbind(data_cleaned$V3, data_cleaned$V4)
+    }, ignoreNULL = FALSE)
+    
   output$mymap <- renderLeaflet({leaflet() %>%
       addProviderTiles(providers$Stamen.TonerLite,
                        options = providerTileOptions(noWrap = TRUE)
